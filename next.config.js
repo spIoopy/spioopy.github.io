472e684c0ee71baf36e122b13a8bdeb3bc42cb78
spIoopy <79509425+spIoopy@users.noreply.github.com>
2023-09-02 15:53:19 +0200

    test

---
+++ next.config.js
@@ -0,0 +1,43 @@
+const { withPlausibleProxy } = require('next-plausible')
+
+module.exports = withPlausibleProxy()({
+  customDomain: 'http://think.tsukim.io',
+  scriptName: 'thinking',
+})
+
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.js",
+  staticImage: true,
+  flexsearch: {
+    codeblocks: false,
+  },
+  defaultShowCopyCode: true,
+});
+
+module.exports = withNextra({
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+  redirects: () => {
+    return [
+      {
+        source: "/faq",
+        destination: "/faq/getting-started",
+        statusCode: 301,
+      },
+      {
+        source: "/faq",
+        destination: "/faq/getting-started",
+        statusCode: 302,
+      },
+      {
+        source: "/examples",
+        destination: "/examples/basic",
+        statusCode: 302,
+      },
+    ];
+  },
+  reactStrictMode: true,
+});
